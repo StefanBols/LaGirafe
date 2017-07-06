406f1d96bd7881955da4705f574893c4e1a50d0e
Stefan Bols <stefan@imbols.net>
2017-07-06 11:27:59 +0200

    inject script is now searching for an input which contains "name" for the username and "pass" for the password. This resolves #1

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,16 +1,22 @@
-(function() {
-	chrome.storage.sync.get({
-		username: "",
-		password: ""
-	}, function(items) {
-		var usr = items.username;
-		var pwd = items.password;
+(function () {
+    chrome.storage.sync.get({
+        username: "",
+        password: ""
+    }, function (items) {
+        var usr = items.username;
+        var pwd = items.password;
 
-		if (usr !== "" && pwd !== "") {
-			var form = document.forms["loginForm"];
-			document.forms['loginForm'].elements["username"].value = usr;
-			document.forms['loginForm'].elements["password"].value = pwd;
-			document.getElementsByTagName("button")[0].click();
-		}
-	});
+        if (usr !== "" && pwd !== "") {
+            var inputUsr = document.querySelectorAll('input[name*=name]')[0];
+            var inputPwd = document.querySelectorAll('input[name*=pass]')[0];
+
+            var btnSubmit;
+            btnSubmit = document.querySelectorAll('input[type=submit]')[0];
+            if (!btnSubmit) btnSubmit = document.getElementsByTagName("button")[0];
+
+            inputUsr.value = usr;
+            inputPwd.value = pwd;
+            btnSubmit.click();
+        }
+    });
 })();
