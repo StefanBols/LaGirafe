214efc61b6be3ff5570ba19cbe8496e59451352a
Stefan Bols <stefan@imbols.net>
2017-07-06 18:46:00 +0200

    Added support for other login sites than Umbraco

--- scripts/load.js
+++ scripts/load.js
@@ -1,7 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
-    if (tab.url.indexOf("/umbraco/") != -1) { 
-        chrome.tabs.executeScript(tab.ib, {
-            file: 'scripts/inject.js'
-        });
-    }  
+    chrome.tabs.executeScript(tab.ib, {
+        file: 'scripts/inject.js'
+    });
 });
